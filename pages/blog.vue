e7dab0be6c0c1caaa057141fd6fa789c4e24cefa
asagiman <lagavulin.jb.dc@gmail.com>
2022-08-17 23:06:11 +0900

    0817

---
+++ pages/blog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1>ブログページ</h1>
+    <p class="text">こんにちは</p>
+    </div>
+
+</template>
+
+<script setup> // データの取得や操作など、このページで実行したい処理のJSコードを書く
+
+
+</script>
+
+<style>
+    h1{
+        color: red;
+    }
+
+    .text{
+        color:green;
+    }
+</style>
