deeb184bd1abd80a0ed8766b3ba62f12fef4b848
asagiman <lagavulin.jb.dc@gmail.com>
2022-08-25 01:00:30 +0900

    １

---
+++ pages/blog/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+        <img src="data.image" alt="blog-image">
+    </div>
+    <div>
+        <div>
+            <h1>{{data.title}}</h1>
+            <p>{{data.date}}</p>
+            <ContentDoc />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData(useRoute().path, () => 
+    queryContent(useRoute().path).findOne()
+)
+</script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,20 @@
 <template>
+    <div>
+        <div>
+            <h1>Contact</h1>
+            <p>お気軽にご連絡ください</p>
+            <form method = "POST">
+                <label htmlFor = "name">お名前</label>
+            </form>
+
+
+
+
+
+        </div>
+    </div>
+
+
+
     <h1>Contactページ</h1>
 </template>
