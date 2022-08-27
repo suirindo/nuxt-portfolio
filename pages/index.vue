b164c9837d18027823189204d63cd6d44b1b62f0
asagiman <lagavulin.jb.dc@gmail.com>
2022-08-28 08:54:18 +0900

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <div class="hero">
-            <img src="~/assets/images/index-hero.jpg" alt="hero">
+            
             <div class="textContainer">
                 <h1>Tomohiro Iida</h1>
-                <h3>JavaScript Developer</h3>
+                <h3>Human Resource Professional</h3>
             </div>
         </div>
         <div class="container">
             <div class="profile">
                 <div>
-                    <h2>JavaScript Nerd</h2>
+                    <h2></h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <img src="~/assets/images/profile.jpg" alt="profile">
